980e6be66dc141ac720f827d41cf227ef4f9f7b7
Cortiis <tomar.vivek@gmail.com>
2016-11-10 16:17:10 +0530

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,4 +1,12 @@
 <?php
+class execute extends PHPUnit_Framework_TestCase
+{
+	public function testOnePlusOne() {
+		$this->assertEquals(1+1,2);
+  	}
+}
+
+
 //$path = '/home/travis/build/Cortiis/ProDocs/EasyLoaderHelp';
 
 //$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
