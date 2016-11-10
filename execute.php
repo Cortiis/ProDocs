da3dbbcb5727427152c762337d029d4a7fceea9a
Cortiis <tomar.vivek@gmail.com>
2016-11-10 15:42:46 +0530

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,3 +1,11 @@
 <?php
+$path = realpath('/EasyLoaderHelp');
 
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+foreach($objects as $name => $object){
+    //echo "$name\n";
+  $file = fopen("test.txt","w");
+echo fwrite($file,$name\n");
+fclose($file);
+}
 ?>
